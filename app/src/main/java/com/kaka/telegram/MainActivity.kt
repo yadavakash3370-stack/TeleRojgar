4bb0c53c663dbddace4325ca400b753f26d44562
yadavakash3370-stack <yadavakash3370@gmail.com>
2026-09-10 20:32:11 +0530

    Add MainActivity for Telegram app

---
+++ app/src/main/java/com/kaka/telegram/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kaka.telegram
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(
+            android.R.layout.simple_list_item_1
+        )
+    }
+}
