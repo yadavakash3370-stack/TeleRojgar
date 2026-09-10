9d6fedd63a725c2b6c3ecfe9b7dac818c46b5543
yadavakash3370-stack <yadavakash3370@gmail.com>
2026-09-10 20:56:33 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,4 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
 
     // TDLib Android
-    implementation("io.github.tdlib-android:core:0.1.0")
-    implementation("io.github.tdlib-android:ktx:0.1.0")
 }
