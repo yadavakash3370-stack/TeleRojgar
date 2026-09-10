a827be273d8a5c2436e4c5144146ec8af88f7415
yadavakash3370-stack <yadavakash3370@gmail.com>
2026-09-10 20:30:48 +0530

    Initialize Android project with build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.kaka.telegram"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.kaka.telegram"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        buildConfigField(
+            "int",
+            "TELEGRAM_API_ID",
+            "0"
+        )
+
+        buildConfigField(
+            "String",
+            "TELEGRAM_API_HASH",
+            "\"YOUR_API_HASH\""
+        )
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+
+    // TDLib Android
+    implementation("io.github.tdlib-android:core:0.1.0")
+    implementation("io.github.tdlib-android:ktx:0.1.0")
+}
